214401a28d56efd9033661b5ed3a6912fe341150
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-10-15 22:05:56 -0300

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,32 @@
+public class Question1_clinica_paciente {
+	
+	    public static void main(String[] args){
+	
+	        Q1_paciente n1 = new Q1_paciente(1, "Dhomini Andrade dos Santos", "26/02/2004", "Masculino", "Padrão", "Nenhum", "O+");
+	
+	        System.out.println("\nCodigo: "+n1.getCodigo());
+	        System.out.println("Nome: "+n1.getNome());
+	        System.out.println("Data de nascimento: "+n1.getDataNascimento());
+	        System.out.println("Sexo: "+n1.getSexo());
+	        System.out.println("Plano de saúde: "+n1.getPlanoSaude());
+	        System.out.println("Alergia: "+n1.getAlergia());
+	        System.out.println("Tipo Sanguíneo: "+n1.getTipoSanguineo()+"\n");
+	
+	        n1.setNome("Joselina Batista de Andrade");
+	        n1.setDataNascimento("26/09/1980");
+	        n1.setSexo("Feminino");
+	        n1.setAlergia("Nenhuma");
+	        n1.setTipoSanguineo("O-");
+	        n1.setCodigo(2);
+	        n1.setPlanoSaude("Plus");
+	
+	        System.out.println("\nCodigo: "+n1.getCodigo());
+	        System.out.println("Nome: "+n1.getNome());
+	        System.out.println("Data de nascimento: "+n1.getDataNascimento());
+	        System.out.println("Sexo: "+n1.getSexo());
+	        System.out.println("Plano de saúde: "+n1.getPlanoSaude());
+	        System.out.println("Alergia: "+n1.getAlergia());
+	        System.out.println("Tipo Sanguíneo: "+n1.getTipoSanguineo()+"\n");
+	    }
+	}
+
