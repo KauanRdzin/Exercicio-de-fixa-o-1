ebec55d655428726486e9672b61340a65d35dbd2
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-10-15 22:58:47 -0300

    Create Questao5_part2.java

---
+++ Questao5_part2.java
@@ -0,0 +1,21 @@
+public class Question5_part2 {
+
+    public static void main(String[] args){
+
+        Q5_contato roberto = new Q5_contato("Kauan", 99976746);
+
+        kauan.enviar_mensagem("Olá, Kauan, como vai?");
+
+        kauan.getQuant_naolidas();
+
+        kauan.receber_mensagem("Vou bem!!");
+        kauan.receber_mensagem("E você como vai?");
+        kauan.receber_mensagem("E os familiares?");
+        kauan.receber_mensagem("Eu comprei uma moto nova para ir ao meu trabalho");
+        kauan.receber_mensagem("É a melhor moto que já vi na vida");
+
+        kauan.getQuant_msgnaolidas();
+
+        kauan.mensagensNaoLidas();
+
+        kauan.getQuant_msgnaolidas();
