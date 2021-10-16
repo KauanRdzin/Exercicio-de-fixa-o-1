00e7f7c0ca51837bada9ce23b9647872e1b643b8
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-10-15 22:18:56 -0300

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,30 @@
+public class Question3_aviao {
+	
+	    private int velocidade, altitude;
+	
+	    public Q3_aviao(int velocidade, int altitude){
+	
+	        this.velocidade = velocidade;
+	        this.altitude = altitude;
+	    }
+	
+	    public int getVelocidade(){
+	        return this.velocidade;
+	    }
+	
+	    public int getAltitude(){
+	        return this.altitude;
+	    }
+	
+	    public void aumentarVelocidade(int aceleracao){
+	        this.velocidade += aceleracao;
+	    }
+	    public void diminuirVelocidade(int aceleracao){
+	        this.velocidade -= aceleracao;
+	    }
+	
+	    public void aumentarAltitude (int altitude){
+	        this.altitude -= altitude;
+	    }
+	    public void diminuirAltitude (int altitude){
+	        this.altitude -= altitude;
